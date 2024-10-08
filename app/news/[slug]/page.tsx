4e11ce9b1d5ec02556d85c36ae23c3fbb41f208e
Tonosaki <tonosaki914@icloud.com>
2024-10-08 14:56:49 +0900

    ニュースにSSRを適用

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -11,6 +11,8 @@ type Props = {
   };
 };
 
+export const revalidate = 0;
+
 export default async function Page({ params, searchParams }: Props) {
   const data = await getNewsDetail(params.slug, {
     draftKey: searchParams.dk,
--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,7 +4,9 @@ import SearchField from "../_components/SearchField";
 import { NEWS_LIST_LIMIT } from "../_constants";
 import { getNewsList } from "../_libs/microcms";
 
-export default async function NewsPage() {
+export const revalidate = 0;
+
+export default async function Page() {
   const { contents: news, totalCount } = await getNewsList({
     limit: NEWS_LIST_LIMIT,
   });
